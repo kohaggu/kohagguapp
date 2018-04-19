10ac6cb0fa588b038eeb88203da9052183575ba9
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-04-19 17:49:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const keys = require('./config/keys')
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  
+const app = express();
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Method override middleware
+app.use(methodOverride('_method'));
+// Handlebars Middleware
+
+const {
+    truncate,
+    stripTags,
+    formatDate,
+    select
+  } = require('./helpers/hbs');
+  
+app.engine('handlebars', exphbs({
+  helpers:{ // to help the handlebars in formating
+truncate:truncate,
+stripTags:stripTags,
+formatDate:formatDate,
+select:select
+  },
+  defaultLayout:'main'
+}));
+
+//cookie parser and express session middleware
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Set global varsrs
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+  });
+  //set static folder
+  app.use(express.static(path.join(__dirname,'public')));
+
+  const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
