fa259d964fb8262f2af53b24e6404ff0f6bd34a2
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-04-21 02:25:47 +0530

    api key for google and fb is generated

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
 module.exports = {
-    mongoURI:'mongodb://harshit:scooby1234@ds151809.mlab.com:51809/notesdiyo'
-   // googleClientId:'613914265017-21cv99131e3f35j5h2s5a58ne8f20t80.apps.googleusercontent.com',
-   // googleClientSecret:'NIkZqB855SgQTnWLPz3N-mes'
+    mongoURI:'mongodb://harshit:scooby1234@ds151809.mlab.com:51809/notesdiyo',
+   googleClientId:'266784449011-57isrt8sbicred5rtk4krrpic9ja20js.apps.googleusercontent.com',
+   googleClientSecret:'G11DsSy7pCk9s3MU0Oit8BKQ',
+   facebookClientID:'555049248228767',
+   facebookClientSecret:'c0df033214e176888390edfb72e26f3a',
+   callbackURL:'http://localhost:5000/auth/facebook/callback'
 }
