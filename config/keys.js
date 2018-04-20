00ed83665abdc2bdfcac58d14f9c9e352c66d907
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-04-21 01:28:11 +0530

    setting up auth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://harshit:scooby1234@ds257077.mlab.com:57077/social-dev',
+    mongoURI:'mongodb://harshit:scooby1234@ds151809.mlab.com:51809/notesdiyo'
    // googleClientId:'613914265017-21cv99131e3f35j5h2s5a58ne8f20t80.apps.googleusercontent.com',
    // googleClientSecret:'NIkZqB855SgQTnWLPz3N-mes'
 }
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//create schema 
+const UserSchema = new Schema({
+    google:{
+        ID:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    firstName:{
+        type:String,
+    },
+    lastName:{
+        type:String,
+    },
+    image:{
+     type:String
+    }
+    },
+    facebook:{
+        ID:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        firstName:{
+            type:String,
+        },
+        lastName:{
+            type:String,
+        },
+        image:{
+         type:String
+        }
+    }
+});
+
+//create collection and add scehma
+mongoose.model('users',UserSchema)
