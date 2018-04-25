22bb30833276092924239340199bd906e2b231e5
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-04-25 18:19:59 +0530

    about route is added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.get('/', (req,res) => {
     res.render('index/welcome');
-})
+});
+
+router.get('/about',(req,res) => {
+   res.render('index/about');
+});
 
 module.exports = router;
