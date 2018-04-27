44b9a111f5ac8f59e1e1c05bdbfeee2ab4823aac
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-04-28 02:49:03 +0530

    setting up frontend

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI:process.env.MONGO_URI,
+   googleClientId:process.env.GOOGLE_CLIENT_ID,
+   googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+   //facebookClientID:'555049248228767',
+  // facebookClientSecret:'c0df033214e176888390edfb72e26f3a',
+  // callbackURL:'http://localhost:5000/auth/facebook/callback'
+}
