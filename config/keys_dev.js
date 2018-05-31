0c13dbab8beba41b540b64a9cb309c621f6773a5
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-05-31 12:53:23 +0530

    keys_dev fixed

---
+++ config/keys_dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI:'mongodb://harshit:scooby1234@ds151809.mlab.com:51809/notesdiyo',
+   googleClientId:'266784449011-57isrt8sbicred5rtk4krrpic9ja20js.apps.googleusercontent.com',
+   googleClientSecret:'G11DsSy7pCk9s3MU0Oit8BKQ',
+   facebookClientID:'555049248228767',
+   facebookClientSecret:'c0df033214e176888390edfb72e26f3a',
+   callbackURL:'http://localhost:5000/auth/facebook/callback'
+}
