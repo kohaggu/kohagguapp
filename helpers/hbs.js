f4e7050ea4c121f1e625956c4fa2de22a380e8c0
harshitkohli1997 <harshit.kohli1997@gmail.com>
2018-10-07 23:39:21 +0530

    fixed everything

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -30,5 +30,10 @@ module.exports = {
     } else {
       return '';
     }
-  }
+  },
+  ifEquals: function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+}
+  
+
 }
